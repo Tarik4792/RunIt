e7ca9f0dfbd62ad7dd4a6b0e0f85dc7e636eafae
Tarik Kellman <tarik7683@gmail.com>
2026-07-02 06:04:12 -0400

    Defer push notification registration to fix iOS 26 TurboModule launch crash

--- App.js
+++ App.js
@@ -5,13 +5,18 @@ import { router } from 'expo-router';
 
 export default function App() {
   useEffect(() => {
-    registerForPushNotifications();
+    const timer = setTimeout(() => {
+      registerForPushNotifications();
+    }, 3000);
 
     const sub = Notifications.addNotificationResponseReceivedListener(response => {
       const gameId = response.notification.request.content.data?.gameId;
       if (gameId) router.push(`/game/${gameId}`);
     });
 
-    return () => sub.remove();
+    return () => {
+      clearTimeout(timer);
+      sub.remove();
+    };
   }, []);
 }
